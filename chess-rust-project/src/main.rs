c1cad5fd6e5baf1fc113c9957f824b6e4a291065
SebWysz <wyszynskiseb@gmail.com>
2023-03-29 22:00:09 -0500

    Testing merge with seby branch

--- chess-rust-project/src/main.rs
+++ chess-rust-project/src/main.rs
@@ -6,4 +6,5 @@ use chess_rust_project::Test;
 fn main() {
     println!("Hello, world!");
     let test_lib: Test = Test {exam: "Example String".to_string()};
+    // this change was made on seby branch
 }
