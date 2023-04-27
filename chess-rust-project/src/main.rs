55af125cc478da74e624478bea4c23f7b2b57ad5
SebWysz <wyszynskiseb@gmail.com>
2023-04-26 19:39:18 -0500

    Now Highlights selected piece

--- chess-rust-project/src/main.rs
+++ chess-rust-project/src/main.rs
@@ -44,6 +44,9 @@ struct Position {
     y : f32,
 }
 
+#[derive(Component)]
+struct Redtile;
+
 
 fn setup_board(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>, asset_server: Res<AssetServer>) {
     let window = window_query.get_single().unwrap();
@@ -53,12 +56,12 @@ fn setup_board(mut commands: Commands, window_query: Query<&Window, With<Primary
     for row in 0..BOARD_SIZE {
         for col in 0..BOARD_SIZE {
             let tile_color = if (row + col) % 2 == 0 {
-                Color::rgb(0.345098, 0.09412, 0.270588)
+                Color::rgb_u8(150, 77, 34)
             } else {
-                Color::rgb(0.949, 0.8235, 0.741176)
+                Color::rgb_u8(218, 217, 181)
             };
 
-            commands.spawn(SpriteBundle {
+            commands.spawn(SpriteBundle{
                 sprite: Sprite{
                     color: tile_color,
                     rect: Some(Rect::new(0.,0.,TILE_SIZE,TILE_SIZE)),
@@ -67,7 +70,7 @@ fn setup_board(mut commands: Commands, window_query: Query<&Window, With<Primary
                 transform: Transform::from_xyz(
                     col as f32 * TILE_SIZE + horiz_displacement,
                     row as f32 * TILE_SIZE + vert_displacement,
-                    0.0,
+                    -1.0,
                 ),
                 ..default()
             });
@@ -255,8 +258,11 @@ fn mouse_click_system(
     piece_query: Query<(Entity, &mut Transform, &mut Position, &Piece), Without<CurrentSelectedPiece>>,
     mut curr_piece_query: Query<(Entity, &mut Transform, &mut Position, &Piece, &CurrentSelectedPiece)>,
     mut commands: Commands,
+    red_tiles: Query<Entity, With<Redtile>>,
 ) {
     let window = window_query.get_single().unwrap();
+    let horiz_displacement = window.width() / 2. - TILE_SIZE * 3.5;
+    let vert_displacement = window.height() / 2. - TILE_SIZE * 3.5;
     
     if mouse_button_input.just_pressed(MouseButton::Left) {
         let mouse_tile = find_mouse_tile(window.cursor_position().unwrap(), window);
@@ -268,10 +274,10 @@ fn mouse_click_system(
             match curr_piece_query.get_single_mut() {
                 // has piece picked up
                 Ok((curr_entity, mut curr_trans, mut curr_pos, piece_qual, _curr_sel_piece)) => {
-                    if !(valid_tiles(curr_pos.x, curr_pos.y, piece_qual, &piece_query).contains(&mouse_tile)) {
-                        // insert error noise or blinking? to signal wrong move
-                        return;
-                    }
+                    // if !(valid_tiles(curr_pos.x, curr_pos.y, piece_qual, &piece_query).contains(&mouse_tile)) {
+                    //     // insert error noise or blinking? to signal wrong move
+                    //     return;
+                    // }
                     // Then if it is a valid tile, move the piece there
                     for (entity, _transform, position, piece_qual) in piece_query.into_iter() {
                         if position.x == mouse_tile[0] && position.y == mouse_tile[1] {
@@ -286,6 +292,11 @@ fn mouse_click_system(
                     curr_pos.y = mouse_tile[1];
                     curr_trans.translation += direction;
                     commands.entity(curr_entity).remove::<CurrentSelectedPiece>();
+                    if !red_tiles.is_empty() {
+                        for tile in red_tiles.into_iter() {
+                            commands.entity(tile).despawn();
+                        }
+                    }
                 },
                 // no piece picked up
                 Err(_) => 
@@ -293,15 +304,30 @@ fn mouse_click_system(
                     for (entity, _transform, position, piece_qual) in piece_query.into_iter() {
                         if position.x == mouse_tile[0] && position.y == mouse_tile[1] {
                             commands.entity(entity).insert(CurrentSelectedPiece);
+                            commands.spawn((
+                                SpriteBundle{
+                                    sprite: Sprite{
+                                        color: Color::rgb_u8(244, 113, 116),
+                                        rect: Some(Rect::new(0.,0.,TILE_SIZE,TILE_SIZE)),
+                                        ..default()
+                                    },
+                                    transform: Transform::from_xyz(
+                                        mouse_tile[0] * TILE_SIZE + horiz_displacement,
+                                        mouse_tile[1] * TILE_SIZE + vert_displacement,
+                                        0.,
+                                    ),
+                                    ..default()
+                                },
+                                Redtile,
+                            ));
                         }
                     }
             }
-            
         }
     }
 }
 
-fn find_mouse_tile(input : Vec2, window : &Window) -> Vec2 {
+pub fn find_mouse_tile(input : Vec2, window : &Window) -> Vec2 {
     let horiz_displacement = window.width() / 2. - TILE_SIZE * 4.;
     let vert_displacement = window.height() / 2. - TILE_SIZE * 4.;
     Vec2::new(f32::floor((input[0] - horiz_displacement) / TILE_SIZE), 
@@ -310,26 +336,26 @@ fn find_mouse_tile(input : Vec2, window : &Window) -> Vec2 {
 }
 
 // also have to somehow check that the king is not in check --- TBD
-fn valid_tiles(x_curr : f32, y_curr : f32, piece : &Piece, 
-                    piece_query: &Query<(Entity, &mut Transform, &mut Position, &Piece), Without<CurrentSelectedPiece>>
-                ) -> Vec<Vec2> {
-    let mut to_return: Vec<Vec2>;
-    // Now fill to_return with all possible moves
-    // piece_query is all the pieces on the board besides the one we are currently parsing
-    // Look at code above to see 
+// fn valid_tiles(x_curr : f32, y_curr : f32, piece : &Piece, 
+//                     piece_query: &Query<(Entity, &mut Transform, &mut Position, &Piece), Without<CurrentSelectedPiece>>
+//                 ) -> Vec<Vec2> {
+//     let mut to_return: Vec<Vec2>;
+//     // Now fill to_return with all possible moves
+//     // piece_query is all the pieces on the board besides the one we are currently parsing
+//     // Look at code above to see 
+    
+//     // Make sure the tile is not occupied with another piece; if it is: same color = not valid tile, different color = valid tile
+//     // Try to do basic moves first like King, Rook, bishop, or queen, then Knight, then pawn (with double move if it's on its home rank)
+//     // Then do castling and En passant last (text me with questions)
 
-    // Make sure the tile is not occupied with another piece; if it is: same color = not valid tile, different color = valid tile
-    // Try to do basic moves first like King, Rook, bishop, or queen, then Knight, then pawn (with double move if it's on its home rank)
-    // Then do castling and En passant last (text me with questions)
-
-    // I suggest creating a helper function so that you could do (bishop moves + rook moves == Queen Moves) or something like that
-    match piece.piece_type {
-        PieceType::King   =>  todo!(),
-        PieceType::Queen  =>  todo!(),
-        PieceType::Bishop =>  todo!(),
-        PieceType::Rook   =>  todo!(),
-        PieceType::Knight =>  todo!(),
-        PieceType::Pawn   =>  todo!(),
-    };
-    return to_return;
-}
+//     // I suggest creating a helper function so that you could do (bishop moves + rook moves == Queen Moves) or something like that
+//     match piece.piece_type {
+//         PieceType::King   =>  todo!(),
+//         PieceType::Queen  =>  todo!(),
+//         PieceType::Bishop =>  todo!(),
+//         PieceType::Rook   =>  todo!(),
+//         PieceType::Knight =>  todo!(),
+//         PieceType::Pawn   =>  todo!(),
+//     };
+//     return to_return;
+// }
